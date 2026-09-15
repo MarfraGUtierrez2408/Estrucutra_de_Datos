1ea36cd46e32dd2210cf11d4e4084b1d95502743
MarfraGUtierrez2408 <marfra.gutierrez07@gmail.com>
2026-09-15 13:02:15 -0600

    Add files via upload
    Tarea 4, Materias entre 2 estudiantes

--- Main.java
+++ Main.java
@@ -1,13 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.HashSet;
+import java.util.Set;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[]args) {
+        Set<String> marco= new HashSet<>();
+        marco.add("POO");
+        marco.add("GENERO");
+        marco.add("TLRID");
+        marco.add("LABORATORIO");
+        marco.add("BASES DE DATOS");
+//---------------------------------------------------
+        Set<String> sebas = new HashSet<>();
+        sebas.add("POO");
+        sebas.add("MATEMATICAS APLICADAS");
+        sebas.add("PEOGRAMACION WEB");
+        sebas.add("ADMINISTRACION DE PROYECTOS");
+        sebas.add("BASES DE DATOS");
+//.....................................................
+        System.out.println("Materias de Marco: "+marco);
+        System.out.println("Materias de Sebastian: "+sebas);
+
+
+        Set<String> union = new HashSet<>(marco);
+        union.retainAll(sebas);
+        System.out.println("Todas las materias que lleva al mneos uno: "+ union);
+
+        Set<String> comun = new HashSet<>(marco);
+        comun.retainAll(sebas);
+        System.out.println("Materias en comun entre Sebas Y Marco: "+comun);
+
+        Set<String> MarcoSolo = new HashSet<>(marco);
+        MarcoSolo.removeAll(sebas);
+        System.out.println("Materias de Marco: " + MarcoSolo);
+
+        Set<String> SebasSolo = new HashSet<>(sebas);
+        SebasSolo.removeAll(marco);
+        System.out.println("Materias de Sebastian: " + SebasSolo);
+
+        System.out.println("\nMarco cursa Poo? " + marco.contains("POO"));
+        System.out.println("Sebas cursa Genero? " + sebas.contains("GENERO"));
+
+        System.out.println("Total de materias distintas: " + union.size());
+
+
+
     }
 }
